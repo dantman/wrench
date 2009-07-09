7c33554977d64db43ad43f420dd3f313ed237c26
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-08 23:02:14 -0700

    array.reduceNative (So we can do things like [1,2,3].reduceNative(Math.max);) Math.sum and Math.avg

--- src/Math.js
+++ src/Math.js
@@ -7,3 +7,24 @@ Math.rand = function rand(min, max) {
 	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 };
 
+/**
+ * Calculate the sum of a list of numbers. Is also useful as an argument to .reduceNative
+ */
+if(!Math.sum)
+	Math.sum = function sum(/*...*/) {
+		if( !arguments.length )
+			throw new TypeError();
+		var args = Array.slice(arguments);
+		var s = args.shift();
+		while( args.length )
+			s += args.shift();
+		return s;
+	};
+
+/**
+ * Calculate the average value from a list of numbers.
+ */
+Math.avg = function avg(/*...*/) {
+	return Math.sum.apply(Math, arguments) / arguments.length;
+};
+
