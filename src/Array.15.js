5e8e8b80ce255fc8446444b1e71399c972808d8e
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-08 17:48:55 -0700

    .diff() and .intersect() for js1.5

--- src/Array.15.js
+++ src/Array.15.js
@@ -1,12 +1,26 @@
-Array.prototype.diff = function intersect() {
-	
+/**
+ * Compare this array with another one and return an array with all the items
+ * in this one that are not in the other.
+ * 
+ * @param Array otherArray The array to compare to
+ * @return Array An array with all items in this array not in otherArray
+ */
+Array.prototype.diff = function diff(otherArray) {
+	return this.filter(function(item) {
+		return !otherArray.has(item);
+	});
 };
 
-Array.prototype.intersect = function intersect() {
-	
-};
-
-Array.prototype.union = function union() {
-	
+/**
+ * Compare this array with another one and return an array with all the items
+ * that are in both arrays.
+ * 
+ * @param Array otherArray The array to compare to
+ * @return Array An array with all items in this array and otherArray
+ */
+Array.prototype.intersect = function intersect(otherArray) {
+	return this.filter(function(item) {
+		return otherArray.has(item);
+	});
 };
 
