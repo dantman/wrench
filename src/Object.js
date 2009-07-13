964554285147fd6acfba26a042ae4388000ba1b9
Daniel Friesen <github@nadir-seen-fire.com>
2009-07-13 08:35:26 -0700

    Include extra hash like functionality onto Object.

--- src/Object.js
+++ src/Object.js
@@ -1,8 +1,36 @@
-Object.invert = function(obj) {
+Object.invert = function invert(obj) {
 	var o = {};
 	for( var k in obj )
 		if( obj.hasOwnProperty(k) )
 			o[obj[k]] = k;
 	return o;
 };
+
+Object.merge = function merge() {
+	
+};
+
+(function(useCount) {
+	Object.count = function count(obj) {
+		if ( useCount ) return obj.__count__;
+		return Object.keys(obj).length;
+	};
+})(({}).__count__ === 0);
+
+Object.keys = function keys(obj) {
+	var keys = [];
+	for ( var k in obj )
+		if ( obj.hasOwnProperty( k ) )
+			keys.push( k );
+	return keys;
+};
+
+Object.values = function values(obj) {
+	var values = [];
+	for ( var k in obj )
+		if ( obj.hasOwnProperty( k ) )
+			values.push( obj[k] );
+	return values;
+};
+
 
