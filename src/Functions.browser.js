784c992400082045909ee98623796476b8659d37
Daniel Friesen <github@nadir-seen-fire.com>
2009-08-19 09:17:07 -0700

    Use `this` rather than `window` for more portability.

--- src/Functions.browser.js
+++ src/Functions.browser.js
@@ -1,24 +1,24 @@
 (function(toString) {
 	
-	window.isString = function isString(str) {
+	this.isString = function isString(str) {
 		return typeof str === 'string' || toString(str) === '[object String]';
 	};
 
-	window.isNumber = function isNumber(num) {
+	this.isNumber = function isNumber(num) {
 		return typeof num === 'number' || toString(num) === '[object Number]';
 	};
 
-	window.isArray = function isArray(arr) {
+	this.isArray = function isArray(arr) {
 		return toString(arr) === '[object Array]';
 	};
 
-	window.isObject = function isObject(obj) {
+	this.isObject = function isObject(obj) {
 		return toString(obj) === '[object Object]';
 	};
 
-	window.isFunction = function isFunction(fn) {
+	this.isFunction = function isFunction(fn) {
 		return toString(fn) === '[object Function]';
 	};
 	
-})(Object.prototype.toString);
+}).call(this, Object.prototype.toString);
 
