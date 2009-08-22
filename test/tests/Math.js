075882834ce63b606f85847a66197c2ed82acc5c
Daniel Friesen <github@nadir-seen-fire.com>
2009-08-21 22:20:50 -0700

    Fix math test.

--- test/tests/Math.js
+++ test/tests/Math.js
@@ -2,8 +2,8 @@
 exports["Math.rand"] = function() {
 	for(var i = 0; i < 500; i++ ) {
 		var r = Math.rand(5, 25);
-		assert(r <= 5, "Math.rand outputted a number that is to small");
-		assert(r >= 25, "Math.rand outputted a number that is to large");
+		assert(r >= 5, "Math.rand outputted a number that is to small");
+		assert(r <= 25, "Math.rand outputted a number that is to large");
 		assert(Math.floor(r) === r, "Math.rand outputted a non-integer");
 	}
 };
