34b3bb79b22cb0804b1fcec08cfb65aa1a69ad1a
Daniel Friesen <github@nadir-seen-fire.com>
2010-02-13 13:31:24 -0800

    Use tabs not spaces.

--- src/isodate.js
+++ src/isodate.js
@@ -4,11 +4,11 @@ if ( isNaN(Date.parse("2000-01-01T00:00:00.000Z")) ) {
 		var Date = function(arg) {
 			if ( this instanceof Date || this instanceof NativeDate ) {
 				var d = isString(arg) ?
-                    // We explicitly pass it through parse:
+					// We explicitly pass it through parse:
 					new NativeDate(Date.parse(arg)) :
-                    // We have to manually make calls depending on argument
-                    // length here
-                    NativeDate.constructor.apply(new NativeDate(), arguments)
+					// We have to manually make calls depending on argument
+					// length here
+					NativeDate.constructor.apply(new NativeDate(), arguments);
 				d.constructor = Date; // Prevent mixups with unfixed Date object
 				return d;
 			}
@@ -21,30 +21,30 @@ if ( isNaN(Date.parse("2000-01-01T00:00:00.000Z")) ) {
 		Date.now = NativeDate.now;
 		Date.UTC = NativeDate.UTC;
 		Date.prototype = NativeDate.prototype;
-        // Upgrade Date.parse to handle the ISO dates we use
-        // TODO review specification to ascertain whether it is
-        // necessary to implement partial ISO date strings.
+		// Upgrade Date.parse to handle the ISO dates we use
+		// TODO review specification to ascertain whether it is
+		// necessary to implement partial ISO date strings.
 		Date.parse = function(string) {
 			var m = /^(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d(?:\.\d+)?)Z$/.exec(string);
 			if ( m ) {
 				m.shift(); // kill the match string
 				for ( var i = 0; i < m.length; i++ ) {
-                    // use parseFloat instead of parseInt so the milliseconds
-                    // don't get stripped
-                    // XXX consider separating the seconds and miliseconds
-                    // to avoid floating point precision error.
+					// use parseFloat instead of parseInt so the milliseconds
+					// don't get stripped
+					// XXX consider separating the seconds and miliseconds
+					// to avoid floating point precision error.
 					m[i] = parseFloat(m[i], 10);
-                    // m[1] is the month. Months are 0-11 in JavaScript
-                    // Date objects, but 1-12 in ISO notation, so we
-                    // decrement.
+					// m[1] is the month. Months are 0-11 in JavaScript
+					// Date objects, but 1-12 in ISO notation, so we
+					// decrement.
 					if ( i === 1 )
 						m[i]--;
 				}
 				if ( m.length > 5 ) {
 					var floor = Math.floor(m[5])
-                    // Because of floating point precision, in one of Dan
-                    // Friesen's tests, 27.677 is being turned into
-                    // 676.9999999999995 instead of 677
+					// Because of floating point precision, in one of Dan
+					// Friesen's tests, 27.677 is being turned into
+					// 676.9999999999995 instead of 677
 					m[6] = Math.round((m[5] - floor) * 1000);
 					m[5] = floor;
 				}
@@ -54,9 +54,9 @@ if ( isNaN(Date.parse("2000-01-01T00:00:00.000Z")) ) {
 		};
 		global.Date = Date;
 	})(
-        Date,
-        this.Date === Date ? // this === global ?
-        this : undefined // XXX where is "global" if "this" !== "global" ?
-        // (Helma NG, GPSEE, possibly Flusspferd)
-    );
+		Date,
+		this.Date === Date ? // this === global ?
+		this : undefined // XXX where is "global" if "this" !== "global" ?
+		// (Helma NG, GPSEE, possibly Flusspferd)
+	);
 }
